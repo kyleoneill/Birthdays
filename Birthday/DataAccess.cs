40160132e321720791c340b8ccce7233841c9b7e
Kyle O'Neill <koneill@rationalenterprise.com>
2019-02-14 13:53:16 -0500

    Re-creating project as a UWP

---
+++ Birthday/DataAccess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace Birthday
+{
+    public static class DataAccess
+    {
+        public static void InitializeDatabase()
+        {
+            using (SqliteConnection db =
+                new SqliteConnection("Filename=birthday.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT EXISTS birthdayTable (Primary_Key INTEGER PRIMARY KEY, name VARCHAR, birthday DATE)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+    }
+}
